9d2c8ac7e44db049df16cd8e6733f244ad5b482e
James Kane <james.kane59@outlook.com>
2022-12-24 10:32:08 +0000

    get hourly weather data (temps, time, sky coverage) by location

--- src/API.js
+++ src/API.js
@@ -14,53 +14,90 @@ async function fetchDataByLocation(location) {
 
 	return data;
 }
+
+async function getHourlyInfo(location, units) {
+	const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=${units}&cnt=24&appid=${API_KEY}`;
+
+	const response = await fetch(forecastUrl);
+	const data = await response.json();
+	console.log(data.list);
+
+	// Calculate the current unix timestamp in seconds for the current item
+	const currentTimestamp = Math.floor(Date.now() / 1000);
+
+	const hourlyData = {
+		hourlyTemps: [],
+		hourlyTimes: [],
+        hourlySkies: '',
+	};
+
+	for (let hour of data.list) {
+		if (hour.dt >= currentTimestamp && hour.dt < currentTimestamp + 86400) {
+			hourlyData['hourlyTemps'].push(hour.main.temp);
+			hourlyData['hourlyTimes'].push(moment.unix(hour.dt).format('ha'));
+            hourlyData['hourlySkies'] = hour['weather'][0].main;
+		}
+	}
+
+	console.log(hourlyData);
+
+	return hourlyData;
+}
+
 async function getCurrentWeather(location) {
-    const _data = await fetchDataByLocation(location);
-    const _currentWeather = _data['weather'][0].main;
-    const _skies = _data['weather'][0].description;
+	const _data = await fetchDataByLocation(location);
+	const _currentWeather = _data['weather'][0].main;
+	const _skies = _data['weather'][0].description;
 
-    console.log('Current Weather', _currentWeather);
-    console.log('Skies', _skies);
+	console.log('Current Weather', _currentWeather);
+	console.log('Skies', _skies);
 
-    return [_currentWeather, _skies];
+	return [_currentWeather, _skies];
 }
 
 async function getWindSpeed(location) {
 	const _data = await fetchDataByLocation(location);
 	const windSpeed = _data['wind'].speed;
 	console.log('wind', windSpeed);
-    return windSpeed;
+	return windSpeed;
 }
 
 async function getRealFeel(location) {
-    const _data = await fetchDataByLocation(location);
-    const _realFeel = _data['main'].feels_like;
-    console.log('Real Feel', _realFeel);
-    return _realFeel;
+	const _data = await fetchDataByLocation(location);
+	const _realFeel = _data['main'].feels_like;
+	console.log('Real Feel', _realFeel);
+	return _realFeel;
 }
 
 async function getHumidity(location) {
-    const _data = await fetchDataByLocation(location);
-    const humidity = _data['main'].humidity;
-    console.log('humidity', humidity);
-    return humidity;
+	const _data = await fetchDataByLocation(location);
+	const humidity = _data['main'].humidity;
+	console.log('humidity', humidity);
+	return humidity;
 }
 
 async function getTempMinAndMax(location) {
-    const _data = await fetchDataByLocation(location);
-    const _tempMin = _data['main'].temp_min;
-    const _tempMax = _data['main'].temp_max;
+	const _data = await fetchDataByLocation(location);
+	const _tempMin = _data['main'].temp_min;
+	const _tempMax = _data['main'].temp_max;
 }
 
 async function getTime(location) {
-    const _data = await fetchDataByLocation(location);
-    const _timestamp = _data['dt'];
-    const _time = moment.unix(_timestamp).format('MMMM D, YYYY h:mma');
-    console.log(_time);
+	const _data = await fetchDataByLocation(location);
+	const _timestamp = _data['dt'];
+	const _time = moment.unix(_timestamp).format('MMMM D, YYYY h:mma');
+	console.log(_time);
 
-    return _time;
+	return _time;
 }
 
-
-
-export { getCurrentWeather, fetchDataByLocation, getWindSpeed, getHumidity, getTempMinAndMax, getTime, getRealFeel };
+export {
+	getHourlyInfo,
+	getCurrentWeather,
+	fetchDataByLocation,
+	getWindSpeed,
+	getHumidity,
+	getTempMinAndMax,
+	getTime,
+	getRealFeel,
+};
