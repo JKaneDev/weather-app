1d312413841a041978347180001fc3e226270372
James Kane <james.kane59@outlook.com>
2022-12-28 20:00:18 +0000

    displaying forecast info on screen upon input, including hourly forecast with icon

--- src/API.js
+++ src/API.js
@@ -31,16 +31,21 @@ async function getHourlyInfo(location, units) {
 			const threeHour = {
 				temp: hour['main'].temp.toFixed(0),
 				time: moment.unix(hour.dt).format('ha'),
-				forecast: hour['weather'][0].main
-			}
+				forecast: hour['weather'][0].main,
+				tempMin: hour['main'].temp_min.toFixed(0),
+				icon: hour['weather'][0].icon
+			};
 			hourlyData.push(threeHour);
 		}
 	}
 	return hourlyData;
 }
 
-async function getWeatherIcon(location, units) {
-	const forecast = await fetchDataByLocation(location);
+async function getLocation(location) {
+	const data = await fetchDataByLocation(location);
+	const locationName = data.name;
+
+	return locationName;
 }
 
 async function getDateAndTime(location) {
@@ -70,33 +75,32 @@ async function getDailyInfo(location, units) {
 	const response = await fetch(forecastUrl);
 	const data = await response.json();
 
-    const weeklyData = [];
+	const weeklyData = [];
 
-    for (let i = 0; i < data.list.length; i++) {
-        if (i % 8 === 0) {
-            const dailyData = {
-                day: moment.unix(data.list[i].dt).format('ddd'),
-                temp: data.list[i]['main'].temp,
-                weather: data.list[i]['weather'][0].main,
-                icon: data.list[i]['weather'][0].icon,
-            }
-            weeklyData.push(dailyData);
-        }
-
-    }
-    return weeklyData;
+	for (let i = 0; i < data.list.length; i++) {
+		if (i % 8 === 0) {
+			const dailyData = {
+				day: moment.unix(data.list[i].dt).format('ddd'),
+				temp: data.list[i]['main'].temp,
+				weather: data.list[i]['weather'][0].main,
+				icon: data.list[i]['weather'][0].icon,
+			};
+			weeklyData.push(dailyData);
+		}
+	}
+	return weeklyData;
 }
 
 async function getTempMin(location) {
 	const data = await fetchDataByLocation(location);
-	const tempMin = data['main'].temp_min.toFixed(1);
+	const tempMin = data['main'].temp_min.toFixed(0);
 
 	return tempMin;
 }
 
 async function getTempMax(location) {
 	const data = await fetchDataByLocation(location);
-	const tempMax = data['main'].temp_max.toFixed(1);
+	const tempMax = data['main'].temp_max.toFixed(0);
 
 	return tempMax;
 }
@@ -110,39 +114,129 @@ async function getCurrentWeather(location) {
 
 async function getWindSpeed(location) {
 	const data = await fetchDataByLocation(location);
-	const windSpeed = data['wind'].speed;
-	
-	
+	const windSpeed = data['wind'].speed.toFixed(1);
+
 	return windSpeed;
 }
 
 async function getRealFeel(location) {
 	const data = await fetchDataByLocation(location);
-	const realFeel = data['main'].feels_like;
-	
+	const realFeel = data['main'].feels_like.toFixed(0);
+
 	return realFeel;
 }
 
 async function getHumidity(location) {
 	const data = await fetchDataByLocation(location);
 	const humidity = data['main'].humidity;
-	
+
 	return humidity;
 }
 
-async function getTempMinAndMax(location) {
+async function getIconCode(location) {
 	const data = await fetchDataByLocation(location);
-	const tempMin = data['main'].temp_min;
-	const tempMax = data['main'].temp_max;
+	const icon = data.weather[0].icon;
+
+	return icon;
 }
 
-async function getTime(location) {
-	const data = await fetchDataByLocation(location);
-	const timestamp = data['dt'];
-	const time = moment.unix(_timestamp).format('MMMM D, YYYY h:mma');
-	
+async function getWeatherIcon(iconCode) {
+	const icon = iconCode;
+	let iconObject;
 
-	return time;
+	switch (icon) {
+		case '01d':
+			iconObject = await fetch('https://openweathermap.org/img/wn/01d@2x.png');
+			break;
+		case '01n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/01n@2x.png'
+			);
+			break;
+		case '02d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/02d@2x.png'
+			);
+			break;
+		case '02n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/02n@2x.png'
+			);
+			break;
+		case '03d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/03d@2x.png'
+			);
+			break;
+		case '03n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/03n@2x.png'
+			);
+			break;
+		case '04d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/04d@2x.png'
+			);
+			break;
+		case '04n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/04n@2x.png'
+			);
+			break;
+		case '09d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/09d@2x.png'
+			);
+			break;
+		case '09n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/09n@2x.png'
+			);
+			break;
+		case '10d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/10d@2x.png'
+			);
+			break;
+		case '10n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/10n@2x.png'
+			);
+			break;
+		case '11d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/11d@2x.png'
+			);
+			break;
+		case '11n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/11n@2x.png'
+			);
+			break;
+		case '13d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/13d@2x.png'
+			);
+			break;
+		case '13n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/13n@2x.png'
+			);
+			break;
+		case '50d':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/50d@2x.png'
+			);
+			break;
+		case '50n':
+			iconObject = await fetch(
+				'https://openweathermap.org/img/wn/50n@2x.png'
+			);
+			break;
+	}
+
+
+	return iconObject.url
 }
 
 export {
@@ -152,11 +246,12 @@ export {
 	fetchDataByLocation,
 	getWindSpeed,
 	getHumidity,
-	getTempMinAndMax,
-	getTime,
 	getRealFeel,
 	getCurrentTemp,
 	getDateAndTime,
 	getTempMin,
-	getTempMax
+	getTempMax,
+	getLocation,
+	getWeatherIcon,
+	getIconCode
 };
--- src/Display.js
+++ src/Display.js
@@ -1,43 +1,95 @@
-import { logPlugin } from '@babel/preset-env/lib/debug.js';
 import {
-    getHourlyInfo,
+	getHourlyInfo,
 	getDailyInfo,
-	getCurrentWeather,
 	fetchDataByLocation,
+	getCurrentWeather,
 	getWindSpeed,
 	getHumidity,
-	getTempMinAndMax,
-	getTime,
 	getRealFeel,
-    getCurrentTemp,
-    getDateAndTime,
-    getTempMin,
-    getTempMax
+	getCurrentTemp,
+	getDateAndTime,
+	getTempMin,
+	getTempMax,
+	getLocation,
+	getIconCode,
+	getWeatherIcon,
 } from './API.js';
+import humidity from '../assets/humidity.png';
+import tempMin from '../assets/temp-min.png';
+import tempMax from '../assets/temp-max.png';
+import realFeel from '../assets/outdoor-feel.png';
+import windSpeed from '../assets/anemometer.png';
+import defaultWeather from '../assets/overcast.png';
 
 export const addListeners = () => {
-    document.getElementById('search-button').addEventListener('click', getForecastFromInput);
-    document.getElementById('search-box').addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') getForecastFromInput();
-    });
-}
+	document
+		.getElementById('search-button')
+		.addEventListener('click', getForecastFromInput);
+	document.getElementById('search-box').addEventListener('keydown', (e) => {
+		if (e.key === 'Enter') getForecastFromInput();
+	});
+};
+
+export const loadImages = () => {
+	document.getElementById('current-weather').src = defaultWeather;
+	document.getElementById('humidity-img').src = humidity;
+	document.getElementById('temp-min-img').src = tempMin;
+	document.getElementById('temp-max-img').src = tempMax;
+	document.getElementById('real-feel-img').src = realFeel;
+	document.getElementById('wind-speed-img').src = windSpeed;
+};
 
 const getForecastFromInput = async () => {
-    const inputString = document.getElementById('search-box').value;
-    const location = await fetchDataByLocation(inputString);
-    const currentWeather = await getCurrentWeather(inputString);
-    const description = getCurrentWeather(inputString);
-    const currentTemp = await getCurrentTemp(inputString, 'metric');
-    const dateAndTime = await getDateAndTime(inputString);
-    const feelsLike = await getRealFeel(inputString);
-    const humidity = await getHumidity(inputString);
-    const tempMin = await getTempMin(inputString);
-    const tempMax = await getTempMax(inputString);
-    const windSpeed = await getWindSpeed(inputString);
-    const hourlyData = await getHourlyInfo(inputString, 'metric');
-    const dailyData = await getDailyInfo(inputString);
+	//fetch all forecast info
+	const inputString = document.getElementById('search-box').value;
+	const location = await getLocation(inputString);
+	const description = await getCurrentWeather(inputString);
+	const currentTemp = await getCurrentTemp(inputString, 'metric');
+	const iconCode = await getIconCode(inputString);
+	const weatherIcon = await getWeatherIcon(iconCode);
+	const dateAndTime = await getDateAndTime(inputString);
+	const feelsLike = await getRealFeel(inputString);
+	const humidity = await getHumidity(inputString);
+	const tempMin = await getTempMin(inputString);
+	const tempMax = await getTempMax(inputString);
+	const windSpeed = await getWindSpeed(inputString);
+	const hourlyData = await getHourlyInfo(inputString, 'metric');
+	const dailyData = await getDailyInfo(inputString);
 
-    console.log(currentTemp);
-    console.log(location);
-    console.log(hourlyData);
-}
+	//display info on screen
+	document.getElementById('forecast').innerText = `${description}`;
+	document.getElementById('location').innerText = `${location}`;
+	document.getElementById('date').innerText = `${dateAndTime}`;
+	document.getElementById('temp').innerText = `${currentTemp}\u00B0C`;
+	document.getElementById('current-weather').src = `${weatherIcon}`;
+	document.getElementById('feel').innerText = `${feelsLike}\u00B0C`;
+	document.getElementById('humidity').innerText = `${humidity}%`;
+	document.getElementById('temp-min').innerText = `${tempMin}\u00B0C`;
+	document.getElementById('temp-max').innerText = `${tempMax}\u00B0C`;
+	document.getElementById('wind-speed').innerText = `${windSpeed}km/h`;
+
+	for (let data of hourlyData) {
+		let wrapper = document.createElement('div');
+		wrapper.classList.add('hour-wrapper');
+
+		let hour = document.createElement('p');
+		hour.innerText = data.time;
+		hour.classList.add('hourly-time');
+
+		let temp = document.createElement('p');
+		temp.innerText = `${data.temp}\u00B0C`;
+		temp.classList.add('hourly-temp');
+
+		let icon = document.createElement('img');
+        icon.classList.add('hourly-icon');
+        icon.src = await getWeatherIcon(data.icon);
+
+		wrapper.appendChild(hour);
+		wrapper.appendChild(temp);
+		wrapper.appendChild(icon);
+
+        document.getElementById('daily-hourly-wrapper').appendChild(wrapper);
+	}
+
+	console.log(hourlyData);
+};
